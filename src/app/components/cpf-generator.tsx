32e1e6d812561724a8e8c19bee9d7fc9c8182c9c
Erika Rocha <erika.rocha@zig.tickets>
2024-08-02 17:31:06 -0300

    final adjustments

---
+++ src/app/components/cpf-generator.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import React, { useState } from "react";
+import { Alert } from "reactstrap";
+import copy from "../../../public/copy.svg";
+import sync from "../../../public/sync.svg";
+import Image from "next/image";
+
+const generateCPF = (): string => {
+  const randomInt = (min: number, max: number): number =>
+    Math.floor(Math.random() * (max - min + 1)) + min;
+
+  const generateDigit = (base: number[]): number => {
+    const sum = base.reduce(
+      (acc, curr, index) => acc + curr * (base.length + 1 - index),
+      0
+    );
+    const remainder = sum % 11;
+    return remainder < 2 ? 0 : 11 - remainder;
+  };
+
+  const base = Array.from({ length: 9 }, () => randomInt(0, 9));
+  const digit1 = generateDigit(base);
+  const digit2 = generateDigit([...base, digit1]);
+
+  return `${base.join("")}${digit1}${digit2}`;
+};
+
+const formatCPF = (cpf: string): string => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+};
+
+const CPFGenerator: React.FC = () => {
+  const [cpf, setCpf] = useState<string>(generateCPF());
+  const [isFormatted, setIsFormatted] = useState<boolean>(true);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+
+  const copyToClipboard = () => {
+    const cpfToCopy = isFormatted ? formatCPF(cpf) : cpf;
+    navigator.clipboard.writeText(cpfToCopy).then(() => {
+      setShowAlert(true);
+      setTimeout(() => setShowAlert(false), 3000);
+    });
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            padding: "1rem",
+            marginRight: "1rem",
+          }}
+        >
+          <div
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              marginBottom: "1rem",
+              gap: "2rem",
+            }}
+          >
+            <input
+              type="text"
+              value={isFormatted ? formatCPF(cpf) : cpf}
+              readOnly
+              style={{
+                fontSize: "1.3rem",
+                padding: "0.5rem",
+                borderRadius: "0.5rem",
+                textAlign: "center",
+                width: "250px",
+              }}
+            />
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "1rem",
+              }}
+            >
+              <button
+                onClick={() => setCpf(generateCPF())}
+                style={{ width: 40, height: 40 }}
+              >
+                <Image src={sync} alt="Gerar" width={30} height={30} />
+              </button>
+              <button
+                onClick={copyToClipboard}
+                style={{ width: 40, height: 40 }}
+              >
+                <Image src={copy} alt="Copiar" width={30} height={30} />
+              </button>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "inline-flex",
+              alignItems: "center",
+              marginBottom: "1rem",
+              padding: "0.5rem",
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={isFormatted}
+              onChange={() => setIsFormatted(!isFormatted)}
+              style={{
+                width: "80px",
+                height: "40px",
+              }}
+            />
+            <label style={{ marginLeft: "-3rem" }}>Com pontuação</label>
+          </div>
+        </div>
+      </div>
+      {showAlert && (
+        <Alert color="success" style={{ marginBottom: "1rem" }}>
+          <span className="alert-icon">
+            <i className="ni ni-like-2"></i>
+          </span>
+          <strong>Copied to clipboard!</strong>
+        </Alert>
+      )}
+    </div>
+  );
+};
+
+export default CPFGenerator;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,93 +1,39 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import CPFGenerator from "./components/cpf-generator";
+import styles from "./style/page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className={styles.code}>
+        <p>Gerador de CPF</p>
+        <div className={styles.description}>
+          <p>By Érika Rocha</p>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+      <CPFGenerator />
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://erikarg.github.io/me/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Portfolio <span>-&gt;</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
+          <p>My main website.</p>
         </a>
-
+        <br />
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="http://www.github.com/erikarg"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Projects <span>-&gt;</span>
           </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          <p>Find more in my github.</p>
         </a>
       </div>
     </main>
